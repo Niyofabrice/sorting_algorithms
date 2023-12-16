09811607d6e011a456817d4d9bec98ab65489af0
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-12-16 03:58:11 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,13 +9,13 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i;
 	int temp = 0;
-	bool flag = true;
+	int flag = 1;
 
 	if (array == NULL || size == 0)
 		return;
 	while (flag)
 	{
-		flag = false;
+		flag = 0;
 		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
@@ -24,7 +24,7 @@ void bubble_sort(int *array, size_t size)
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
 				print_array(array, size);
-				flag = true
+				flag = 1;
 			}
 		}
 	}
