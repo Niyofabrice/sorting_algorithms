799c2ce27eedfc8b5b3928ebc7e14488059aca0c
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-12-18 11:16:34 -0800

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,52 +1,53 @@
 #include "sort.h"
 
 /**
- * swaping_nodes - function that swaps nodes
- * @head: pointer to the head of the list
- * @node1: pointer to the first node
- * @node2: second node
+ * swap - Swap nodes on list.
+ * @list: pointer to head of list.
+ * @current: pointer to current node of list.
+ * @previous: pointer to previous node of list.
  */
-void swaping_nodes(listint_t **head, listint_t **node1, listint_t *node2)
+void swap(listint_t **list, listint_t *current, listint_t *previous)
 {
-	(*node1)->next = node2->next;
-	if (node2->next != NULL)
-	{
-		node2->next->prev = *node1;
-	}
-	node2->prev = (*node1)->prev;
-	node2->next = *node1;
-	if ((*node1)->prev != NULL)
-	{
-		(*node1)->prev->next = node2;
-	}
+	if (current->next)
+		(current->next)->prev = previous;
+
+	current->prev = previous->prev;
+	current->next = previous;
+
+	if (previous->prev)
+		(previous->prev)->next = current;
 	else
-	{
-		*head = node2;
-	}
-	(*node1)->prev = node2;
-	*node1 = node2->prev;
+		(*list) = current;
 }
 
 /**
- * insertion_sort_list - function that sorts list using insertion sort alg
- * @list: the double linked list to sort
+ * insertion_sort_list - Sort a list with insertion sort algorithm.
+ * @list: list of integers
+ * Return: nothing
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t **i, **put, *tmp;
+	listint_t *previous = NULL, *current = NULL, *next = NULL;
 
-	if (list == NULL || *list == NULL || (*list) == NULL)
-	{
+	if (!list || !(*list))
 		return;
-	}
-	for (i = (*list)->next; i != NULL; i = tmp)
+
+	current = (*list)->next;
+	while (current)
 	{
-		tmp = i->next;
-		put = i->prev;
-		while (put != NULL && i->n < put->n)
+		next = current->next;
+		previous = current->prev;
+
+		while (previous && current->n < previous->n)
 		{
-			swaping_nodes(list, &put, i);
-			print_list((const listint_t *)*list);
+			previous->next = current->next;
+
+			swap(list, current, previous);
+			previous->prev = current;
+			previous = current->prev;
+
+			print_list(*list);
 		}
+		current = next;
 	}
 }
