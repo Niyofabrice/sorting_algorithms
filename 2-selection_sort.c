a3bdf1426ae48d91f6de539f522fefcd13c338b7
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-12-18 12:45:57 -0800

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,11 +6,13 @@
  * @size: size of array to be sorted
  * Return: nothing
  */
-void selection_sort(int* array, size_t size)
+void selection_sort(int *array, size_t size)
 {
 	int temp;
 	size_t i, j, min_index;
 
+	if (!array || size < 2)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		min_index = i;
