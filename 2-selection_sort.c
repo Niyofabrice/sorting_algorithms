0069cdbaeeb638f9e023c8ca98b09cb0fb5c654c
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-12-18 12:40:07 -0800

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function that sorts an array using selection sort alg
+ * @array: array to be sorted
+ * @size: size of array to be sorted
+ * Return: nothing
+ */
+void selection_sort(int* array, size_t size)
+{
+	int temp;
+	size_t i, j, min_index;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min_index] > array[j])
+			{
+				min_index = j;
+			}
+		}
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+		}
+	}
+}
